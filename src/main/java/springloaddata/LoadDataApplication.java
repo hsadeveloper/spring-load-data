da4bfabae72ad2b3d4c37c48a013e2472eb6e1e4
Hsabonchi <developersabon@gmail.com>
2023-09-13 12:47:08 -0500

    empty spring boot

---
+++ src/main/java/springloaddata/LoadDataApplication.java
@@ -0,0 +1,14 @@
+package springloaddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadDataApplication.class, args);
+	}
+
+}
+ 	
---
+++ src/test/java/springloaddata/LoadDataApplicationTests.java
@@ -0,0 +1,13 @@
+package springloaddata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
